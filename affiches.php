6cd103bc23ec4ee63648f2fac57f5534b2e30ca8
OLIVIA-RESSY <OLIVIA@OLIVIA.local>
2016-01-21 14:15:13 +0100

    questionnaire en cous

--- affiches.php
+++ affiches.php
@@ -8,8 +8,51 @@
 </head>
 <body>
 	<div>
-		<h1><?php echo $_POST['nom']?></h1>
+		<h1><?php echo $_POST['asso']?></h1>
+		<h1><?php echo $_POST['titre1']?></h1>
+		<h1><?php echo $_POST['titre2']?></h1>
+		<h1><?php echo $_POST['date']?></h1>
+		<h1><?php echo $_POST['adresse']?></h1>
+		<p><?php echo $_POST['presentation']?></p>
 
+		<!-- pour uploder photos -->
+	<?php
+	$dossier = 'uploads/';
+	$fichier = basename($_FILES['logo']['name']);
+	$taille_maxi = 2000000;
+	$taille = filesize($_FILES['logo']['tmp_name']);
+	$extensions = array('.png', '.gif', '.jpg', '.jpeg');
+	$extension = strrchr($_FILES['logo']['name'], '.'); 
+	//Début des vérifications de sécurité...
+	if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
+	{
+	     $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
+	}
+	if($taille>$taille_maxi)
+	{
+	     $erreur = 'Le fichier est trop gros...';
+	}
+	if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+	{
+	     //On formate le nom du fichier ici...
+	     $fichier = strtr($fichier, 
+	          'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
+	          'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+	     $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+	     if(move_uploaded_file($_FILES['logo']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+	     {?>
+	          <img src="<?php echo $dossier.$fichier?>" >
+	     <?php }
+	     else //Sinon (la fonction renvoie FALSE).
+	     {
+	          echo 'Echec de l\'upload !';
+	     }
+	}
+	else
+	{
+	     echo $erreur;
+	}
+?>
 	</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -8,13 +8,46 @@
 </head>
 <body>
 	<!-- Créer le formulaire + je donne une method POST + dans action je lui indique dans quel fichier va s'afficher les données-->
-	<form action="affiches.php" method="POST"> 
+	<form action="affiches.php" method="POST"enctype="multipart/form-data"> 
 <!-- 		<fieldset></fieldset> -->
 		<!-- Insérer les input du formulaire  -->
-		<label for="nom">Nom</label>
-		<input type="text" name="nom">
-		<!-- Bouton Valider le formulaire -->
-		<input type="submit">
+		<ul> 
+			<li> <label for="asso">nom de l'association</label>
+			<input type="text" name="asso"> </li>
+
+			<li> <label for="titre1">titre1</label>
+			<input type="text" name="titre1"> </li>
+
+			<li> <label for="titre2">titre2</label>
+			<input type="text" name="titre2"> </li>
+
+			<li> <label for="date">date et heure</label>
+			<input type="text" name="date"> </li>
+
+			<li> <label for="adresse">adresse</label>
+			<input type="text" name="adresse"> </li>
+			<li> <label for="presentation">Texte de présentation</label>
+       		<br />
+       		<textarea name="presentation" id="presentation" rows="5" cols="30">
+       		</textarea> </li>
+       		<h5> importez ici votre logo </h5>
+			<li>
+			<!-- On limite le fichier à 2Mo -->
+	     	<input type="hidden" name="MAX_FILE_SIZE" value="2000000">
+	     	Fichier : <input type="file" name="logo">
+	     	<input type="submit" name="envoyer" value="Envoyer le fichier">
+	 		</li>
+			<!-- cases à cocher catégories -->
+			<p> categories :<br />
+       <input type="radio" name="categorie" value="salut" id="salut" /> <label for="salut">salut</label><br />
+       <input type="radio" name="categorie" value="bal" id="bal" /> <label for="bal">bal</label><br />
+       <input type="radio" name="categorie" value="loto" id="loto" /> <label for="loto">loto</label><br />
+       <input type="radio" name="categorie" value="kermesse" id="kermesse" /> <label for="kermesse">kermesse</label>
+   </p>
+
+			<!-- Bouton Valider le formulaire -->
+			<input type="submit">
+		</ul> 
 	</form>
 	
 </body>
