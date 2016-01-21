7456f9c03f17eb42c9d312a24d32fb8592623d26
OLIVIA-RESSY <OLIVIA@OLIVIA.local>
2016-01-21 11:14:34 +0100

    formulaire

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php echo test ?>
-<?php echo UNE NOUVELLE MODIF ?>
-ghjhgjhghjgh
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<!-- Créer le formulaire + je donne une method POST + dans action je lui indique dans quel fichier va s'afficher les données-->
+	<form action="affiches.php" method="POST"> 
+<!-- 		<fieldset></fieldset> -->
+		<!-- Insérer les input du formulaire  -->
+		<label for="nom">Nom</label>
+		<input type="text" name="nom">
+		<!-- Bouton Valider le formulaire -->
+		<input type="submit">
+	</form>
+	
+</body>
+</html>
